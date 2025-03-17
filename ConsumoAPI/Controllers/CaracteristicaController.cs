36d8e2376fe2bd1ffb4ae8f2bcd510da9db1f49a
Igor Alves <167634696+IgorHAlves@users.noreply.github.com>
2025-03-17 16:59:15 -0300

    Projeto consumo API Mercado Livre

---
+++ ConsumoAPI/Controllers/CaracteristicaController.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using ConsumoAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumoAPI.Controllers
+{
+    [ApiController]
+    [Route("caracteristica")]
+    public class CaracteristicaController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Caracteristicas()
+        {
+            using var httpClient = new HttpClient();
+            const string endPoint = "https://api.mercadolibre.com/sites/MLB/categories";
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(endPoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    string retornoApi = await response.Content.ReadAsStringAsync();
+
+                    var json = JsonDocument.Parse(retornoApi);
+
+                    var result = json.RootElement.GetProperty("results").EnumerateArray().First();
+
+                    List<Caracteristica> caracteristicas = new List<Caracteristica>();
+
+                    //foreach (var item in result.)
+                    //{
+                    //    caracteristicas.Add(new Caracteristica
+                    //    {
+
+                    //    });
+                    //}
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Ok();
+        }
+    }
+}
---
+++ ConsumoAPI/Models/Caracteristica.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsumoAPI.Models
+{
+    public class Caracteristica
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
